76d102a0644249faa08d96272b912f40abc6a5ae
Prof. Just <olaf.just@w-hs.de>
2018-10-31 14:49:58 +0100

    Add files via upload

--- src/move2goal_Tkinter.py
+++ src/move2goal_Tkinter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# --- move2gola_Tkinter.py ------
+# --- move2goal_Tkinter.py ------
 # Editor Eric
 # sudo apt-get install eric
 # Version vom 31.10.2018 by OJ
@@ -26,6 +26,8 @@ class TurtleClass:
         self.pose_subscriber = rospy.Subscriber('/turtle1/pose',  Pose, self.update_pose)
         self.pose = Pose()
         self.rate = rospy.Rate(10)
+        self.dist_x=0
+        self.dist_y=0
 
     def update_pose(self, data):
         """Callback function which is called when a new message of type Pose is
@@ -33,8 +35,43 @@ class TurtleClass:
 	
         self.pose = data
         self.pose.x = round(self.pose.x, 4)
-        self.pose.y = round(self.pose.y, 4)
-	
+        self.pose.y = round(self.pose.y, 4)       
+        vel_msg = Twist() 
+        rospy.loginfo("Callback %s %s ", self.dist_x, self.dist_y)
+        if(self.dist_y>0 or self.dist_x>0):
+            sollTheta = atan2(self.dist_y,self.dist_x)
+            if abs(self.pose.theta - sollTheta) > 0.015:
+                #Winkel correction
+                if self.pose.theta > math.pi:
+                    self.pose.theta=self.pose.theta-2*math.pi
+                elif self.pose.theta < -math.pi:
+                    self.pose.theta=self.pose.theta+2*math.pi
+
+                # Angular velocity in the z-axis.
+                if self.pose.theta - sollTheta > 0:
+                    vel_msg.angular.z = -0.1
+                else:
+                    vel_msg.angular.z = 0.1
+                rospy.loginfo("Pose is %s", self.pose.theta)
+                rospy.loginfo("Goal pose is %s", sollTheta)
+                rospy.loginfo("Still to Go %s ", abs(self.pose.theta - sollTheta))
+                # Publishing our vel_msg
+                self.velocity_publisher.publish(vel_msg)
+                # Publish at the desired rate.
+                self.rate.sleep()        
+            elif sqrt(pow((start_x -self.pose.x),2)+pow((start_y -self.pose.y),2)) < abs(dist):
+                # Linear velocity in the x-axis.
+                vel_msg.linear.x = 0.2
+                rospy.loginfo("Pose is %s %s", self.pose.x, self.pose.y)
+                rospy.loginfo("Still to Go %s ", dist-sqrt(pow((start_x -self.pose.x),2)+pow((start_y -self.pose.y),2))  )
+                # Publishing our vel_msg
+                self.velocity_publisher.publish(vel_msg)
+                # Publish at the desired rate.
+                self.rate.sleep()
+            # Stopping our robot after the movement is over.
+            vel_msg.linear.x = 0
+            vel_msg.angular.z = 0
+            self.velocity_publisher.publish(vel_msg)	
 
     def euclidean_distance(self, x, y):
         """Euclidean distance between current pose and (x,y)."""
@@ -42,7 +79,6 @@ class TurtleClass:
 
     def way2goX(self, start_x, dist_x):
 	    return abs(start_x + dist_x - self.pose.x)
-
 
     def move(self):
         """Moves the turtle ."""              
@@ -60,51 +96,8 @@ class TurtleClass:
         rospy.loginfo("Start Pose is %s %s", start_x, start_y)
         rospy.loginfo("Way to Go %s ", dist)
         rospy.loginfo("Still to Go %s ", abs(self.pose.theta - sollTheta)  )
-        vel_msg = Twist()
-        while abs(self.pose.theta - sollTheta) > 0.015:
-            #Winkel correction
-            if self.pose.theta > math.pi:
-                self.pose.theta=self.pose.theta-2*math.pi
-            elif self.pose.theta < -math.pi:
-                self.pose.theta=self.pose.theta+2*math.pi
-
-            # Angular velocity in the z-axis.
-            if self.pose.theta - sollTheta > 0:
-                vel_msg.angular.z = -0.1
-            else:
-                vel_msg.angular.z = 0.1
-            rospy.loginfo("Pose is %s", self.pose.theta)
-            rospy.loginfo("Goal pose is %s", sollTheta)
-            rospy.loginfo("Still to Go %s ", abs(self.pose.theta - sollTheta))
-
-            # Publishing our vel_msg
-            self.velocity_publisher.publish(vel_msg)
-            # Publish at the desired rate.
-            self.rate.sleep()
-
-
-        # Stopping our robot after the movement is over.
-        vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
-        self.velocity_publisher.publish(vel_msg)
-
-        while sqrt(pow((start_x -self.pose.x),2)+pow((start_y -self.pose.y),2)) < abs(dist):
-            # Linear velocity in the x-axis.
-            vel_msg.linear.x = 0.2
-            rospy.loginfo("Pose is %s %s", self.pose.x, self.pose.y)
-            rospy.loginfo("Still to Go %s ", dist-sqrt(pow((start_x -self.pose.x),2)+pow((start_y -self.pose.y),2))  )
-            # Publishing our vel_msg
-            self.velocity_publisher.publish(vel_msg)
-            # Publish at the desired rate.
-            self.rate.sleep()
-
-        # Stopping our robot after the movement is over.
-        vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
-        self.velocity_publisher.publish(vel_msg)
-
         # If we press control + C, the node will stop.
-        rospy.spin()
+       #rospy.spin()
 
 # Klasse definieren 
 class Application(Frame):
@@ -137,9 +130,7 @@ class Application(Frame):
         turtle1.dist_x = float(self.goalX.get()) 
         turtle1.dist_y = float(self.goalY.get())      
         turtle1.move()
-
-
-
+        
 if __name__ == '__main__':
     try:
         turtle1 = TurtleClass()
