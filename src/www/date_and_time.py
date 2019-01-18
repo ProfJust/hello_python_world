c1a7bd52d5fcd325234e1f4a46935caccca8be53
Prof. Just <olaf.just@w-hs.de>
2019-01-18 12:21:58 +0100

    Add files via upload

--- src/www/date_and_time.py
+++ src/www/date_and_time.py
@@ -2,27 +2,25 @@
 import datetime
 import	time	
 
+# Ein Objekt mit vorgegeben Datum instanzieren
 myday = datetime.date(1995,1,22)
 print(myday.day,myday.month, myday.year)
 
+# Ein Objekt mit aktuellem Datum instanzieren
 heute = datetime.date.today()
 print(heute)
 
+# Addition von Tagen 
 morgen = heute + datetime.timedelta(1)
 print(morgen)
 
+# Bestimmung von Zeitraeumen in Tagen 
 begin_ss = datetime.date(2019,3,1)
 print("Der Semesterrest in Tagen:")
 rest = begin_ss - heute
 print(rest)
 
-now = datetime.datetime.now()
-print(now)
-print(now.hour)
-print(now.minute)
-print(now.second)
-print(now.microsecond)
-
+# Ein Objekt fuer die Uhrzeit
 now = datetime.datetime.now()
 print(now)
 print(now.hour)
@@ -30,6 +28,15 @@ print(now.minute)
 print(now.second)
 print(now.microsecond) #Systemzeit nicht Uhrzeit
 
+# nochmal - wie lange dauert dass? 
+now = datetime.datetime.now()
+print(now)
+print(now.hour)
+print(now.minute)
+print(now.second)
+print(now.microsecond) 
+
+# Messung eine Zeit
 print("Warte 5 sec")
 time.sleep(5)
 # Beim Aufruf von sleep() nimmt das Betriebssystem den Prozess aus der Menge der
